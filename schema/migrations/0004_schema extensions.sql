0ae13bd586bfb2a7914432b5b61f9101f99e55a3
Jesper Weissglas <jesper@journeyman.se>
2021-08-27 15:53:43 +0200

    idempotent migrations please

--- schema/migrations/0004_schema extensions.sql
+++ schema/migrations/0004_schema extensions.sql
@@ -1,7 +1,7 @@
 ALTER TABLE public.country_production ADD COLUMN IF NOT EXISTS source_operator_name text NULL;
 ALTER TABLE public.country_production ADD COLUMN IF NOT EXISTS oc_operator_id text NULL;
 
-CREATE UNIQUE INDEX sparse_projects_uniq_idx ON public.sparse_projects USING btree (iso3166, project_id);
+CREATE UNIQUE INDEX IF NOT EXISTS sparse_projects_uniq_idx ON public.sparse_projects USING btree (iso3166, project_id);
 ALTER TABLE public.sparse_projects ALTER COLUMN geo_position TYPE geometry USING geo_position::geometry;
 
 ALTER TABLE public.sources ADD COLUMN IF NOT EXISTS int8 integer NULL;
