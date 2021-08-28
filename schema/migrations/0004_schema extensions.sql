d4673d8cb2943ecee01ffa9f0f5963b0b5450679
Jesper Weissglas <jesper@journeyman.se>
2021-08-28 10:40:44 +0200

    New DB Structure

--- schema/migrations/0004_schema extensions.sql
+++ schema/migrations/0004_schema extensions.sql
@@ -4,8 +4,8 @@ ALTER TABLE public.country_production ADD COLUMN IF NOT EXISTS oc_operator_id te
 CREATE UNIQUE INDEX IF NOT EXISTS sparse_projects_uniq_idx ON public.sparse_projects USING btree (iso3166, project_id);
 ALTER TABLE public.sparse_projects ALTER COLUMN geo_position TYPE geometry USING geo_position::geometry;
 
-ALTER TABLE public.sources ADD COLUMN IF NOT EXISTS int8 integer NULL;
-ALTER TABLE public.sources ADD COLUMN IF NOT EXISTS int8 integer NULL;
+ALTER TABLE public.sources ADD COLUMN IF NOT EXISTS records integer NULL;
+ALTER TABLE public.sources ADD COLUMN IF NOT EXISTS data_points integer NULL;
 ALTER TABLE public.sources ADD COLUMN IF NOT EXISTS latest_curation_at timestamp NULL;
 
 UPDATE public.sparse_projects SET production_co2e = 0 WHERE production_co2e IS NULL;
