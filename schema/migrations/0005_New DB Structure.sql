5c457ef76a25473a75911eecdce09a18f6e0bb47
Jesper Weissglas <jesper@journeyman.se>
2021-08-29 17:28:55 +0200

    Fix project sources

--- schema/migrations/0005_New DB Structure.sql
+++ schema/migrations/0005_New DB Structure.sql
@@ -137,31 +137,25 @@ AS $function$
         ORDER BY p.project_identifier;
 $function$;
 
-CREATE OR REPLACE FUNCTION public.get_country_sources(iso3166_ text, iso3166_2_ text DEFAULT ''::text )
+CREATE OR REPLACE FUNCTION public.get_country_sources(iso3166_ text, iso3166_2_ text DEFAULT ''::text)
     RETURNS TABLE(
-                     source_id int4,
-                     "name" text,
-                     name_pretty text,
-                     description text,
-                     url text,
-                     records int8,
-                     data_points int8,
-                     latest_curation_at timestamp,
-                     data_type data_point_type,
-                     quality integer,
-                     grade text
-                 )
+                     source_id integer, name text, name_pretty text, description text, url text,
+                     records bigint, data_points bigint, latest_curation_at timestamp without time zone,
+                     data_type data_point_type, quality integer, grades TEXT[], year integer)
     LANGUAGE sql
     STABLE
 AS $function$
-SELECT DISTINCT s.*, dp.data_type, dp.quality, dp.grade FROM sources s, country_data_point dp
+SELECT DISTINCT s.*, dp.data_type, dp.quality, array_agg(DISTINCT dp.grade), max(dp.YEAR)
+FROM sources s, country_data_point dp
 WHERE s.source_id = dp.source_id AND iso3166_ = dp.iso3166 AND dp.iso3166_2 = iso3166_2_
+GROUP BY s.source_id, dp.data_type, dp.quality
 UNION
-SELECT s.*, 'projection'::data_point_type AS data_type, 1 as quality, 'xp' as grade FROM sources s
+SELECT s.*, 'projection'::data_point_type AS data_type, 1 as quality, array['xp'] as grades, 2020 AS year FROM sources s
 WHERE s.source_id = 100;
 $function$;
 
-CREATE OR REPLACE FUNCTION public.get_project_sources(id integer)
+
+CREATE OR REPLACE FUNCTION public.get_project_sources( for_id integer )
     RETURNS TABLE(
                      source_id int4,
                      "name" text,
@@ -179,7 +173,7 @@ CREATE OR REPLACE FUNCTION public.get_project_sources(id integer)
     STABLE
 AS $function$
 SELECT DISTINCT s.*, dp.data_type, dp.quality, dp.grade FROM sources s, project_data_point dp
-WHERE s.source_id = dp.source_id AND id = dp.project_id
+WHERE s.source_id = dp.source_id AND for_id = dp.project_id
 UNION
 SELECT s.*, 'projection'::data_point_type AS data_type, 1 as quality, 'xp' as grade FROM sources s
 WHERE s.source_id = 100;
